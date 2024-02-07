fe766cf27ae9b79f8ff707ea57b1f50678a22ec3
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-06 22:49:06 -0800

    Create HW2.java

---
+++ HW/HW2.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int badVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
