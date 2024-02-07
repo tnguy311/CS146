0ce0a3bc61074fa6b3f688da4d54ea564a24d64a
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-06 22:55:20 -0800

    Update HW2.py

--- HW/HW2.py
+++ HW/HW2.py
@@ -5,6 +5,5 @@ class Solution:
             mid = left + (right - left) // 2
             if isBadVersion(mid):
                 right = mid
-            else:
-                left = mid + 1
+            else {left = mid + 1}
         return left
