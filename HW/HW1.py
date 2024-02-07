2e245d72a2de1df8e276712fcd67618410e6c2b8
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-06 22:08:53 -0800

    Update HW1.py

--- HW/HW1.py
+++ HW/HW1.py
@@ -2,13 +2,13 @@ def palindrome(s):
     # Convert the string to lowercase
     lowercase_string = s.lower()
     
-    # Remove all non-alphanumeric characters in the string
+    # Remove non-alphanumeric characters 
     alphanumeric_string = ''.join(char for char in lowercase_string if char.isalnum())
     
-    # Check if string is a palindrome
+    # Check string 
     palindrome_check = alphanumeric_string == alphanumeric_string[::-1]
     
-    # Print the result
+    # Print result
     if palindrome_check:
         print("Is the string a palindrome? Yes.")
     else:
