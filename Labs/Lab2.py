1eeee6353feab9a9c57240a6a92c421203c37f3a
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-06 23:52:14 -0800

    Update Lab2.py

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -7,3 +7,9 @@ a = "silent"
 b = "listen"
 result = solution.isAnagram(a, b)
 print(result)
+
+#sorting the characters in both strings and comparing if the sorted versions are equal.
+#In Python implementations, we use the the sorted() function to sort the characters in the strings.
+#We then compare the sorted strings using the equality operator == to check if they are anagrams.
+#If the lengths of the strings are different, they cannot be anagrams, so we return False immediately in that case.
+#Otherwise, we return the result of the comparison. If the sorted strings are equal, it means the strings are anagrams, so we return True; otherwise, False.
