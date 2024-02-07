d46044f8b14f02260200e5a9c36adf8fdd275618
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-06 23:31:44 -0800

    Update HW2.java

--- HW/HW2.java
+++ HW/HW2.java
@@ -12,3 +12,13 @@ public class Solution {
         return left;
     }
 }
+
+
+/* Using binary search algorithm and find the first bad version.
+Initialize two pointers, left and right, representing the range of versions to search within.
+While left < right, calculate the midpoint.
+Then check if mid is a bad version using the isBadVersion API:
+If mid is a bad version, right = mid to search in the left half.
+If mid is not a bad version, left = mid + 1 to search in the right half.
+Repeat the process until left and right converge; left == right.
+Return left, which represents the first bad version.*/
