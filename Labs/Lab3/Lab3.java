4f04f3abc06a80fef74307de4c1f52be56511071
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-12 12:18:49 -0800

    Update Lab3.java

--- Labs/Lab3/Lab3.java
+++ Labs/Lab3/Lab3.java
@@ -1,1 +1,8 @@
-lab 3
+Problem 1:
+  1. n^3 is asymtotically greater than 10000000000n^2
+  2. n^2 log(n) is asymtotically greater than n(log(n))^10
+  3. 2^sqrt(n) is asymtotically greater than nlogn
+  4. 2^2(n) is asymtotically greater than 2^n
+
+Problem 2:
+  
