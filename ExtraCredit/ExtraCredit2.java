551f09368c1ecb9cc38d29efbae6e1eaa5ba4158
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-14 23:23:51 -0800

    Create ExtraCredit2.java

---
+++ ExtraCredit/ExtraCredit2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static long fibonacci(int n) {
+        long[] fib = new long[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(fibonacci(10));
+    }
+}
