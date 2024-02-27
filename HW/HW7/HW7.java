2d76b7f45614f9722c93b14779cb727506474b19
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-26 23:18:23 -0800

    Update HW7.java

--- HW/HW7/HW7.java
+++ HW/HW7/HW7.java
@@ -6,13 +6,13 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // Sort intervals based on start time
         
         PriorityQueue<Integer> rooms = new PriorityQueue<>();
-        rooms.add(intervals[0][1]); // Add the end time of the first meeting
+        rooms.add(intervals[0][1]); // Add end time of the first meeting
 
         for (int i = 1; i < intervals.length; i++) {
             if (rooms.peek() <= intervals[i][0]) {
-                rooms.poll(); // Remove the meeting that has ended
+                rooms.poll(); // Remove meeting ended
             }
-            rooms.add(intervals[i][1]); // Add the end time of the current meeting
+            rooms.add(intervals[i][1]); // Add current meeting end time.
         }
         return rooms.size();
     }
