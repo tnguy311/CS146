3601431353f0673c1c417b15a4ec1280acb054a7
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-02-28 20:31:48 -0800

    Update HW8.py

--- HW/HW8/HW8.py
+++ HW/HW8/HW8.py
@@ -1,13 +1,20 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        f = [[True] * n for _ in range(n)]
-        k, mx = 0, 1
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                f[i][j] = False
-                if s[i] == s[j]:
-                    f[i][j] = f[i + 1][j - 1]
-                    if f[i][j] and mx < j - i + 1:
-                        k, mx = i, j - i + 1
-        return s[k : k + mx]
+    def longestPalindrome(self, s: str) -> int:
+        char_count = {}
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+        
+        length = 0
+        odd_count = False
+        
+        for count in char_count.values():
+            if count % 2 == 0:
+                length += count
+            else:
+                length += count - 1
+                odd_count = True
+        
+        if odd_count:
+            length += 1
+        
+        return length
