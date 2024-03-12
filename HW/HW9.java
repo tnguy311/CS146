38292f8e6a3cc90f0e1ace4e47e36461b4d237b7
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-03-11 22:24:53 -0700

    Create HW9.java

---
+++ HW/HW9.java
@@ -0,0 +1,28 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean validate(TreeNode node, long minVal, long maxVal) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= minVal || node.val >= maxVal) {
+            return false;
+        }
+        return validate(node.left, minVal, node.val) && validate(node.right, node.val, maxVal);
+    }
+}
