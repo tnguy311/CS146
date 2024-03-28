b867d5488bab42ad85b0b2ced890f54e0d74e990
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-03-28 00:20:49 -0700

    Update Lab6.py

--- Labs/Lab6/Lab6.py
+++ Labs/Lab6/Lab6.py
@@ -24,4 +24,4 @@ def canFinish(numCourses, prerequisites):
 #Then check if there are any cycles in the graph by using a recursive DFS approach. Cycles indicate that the courses cannot be completed due to circular dependencies.
 #Then traverse the graph, keeping track of visited nodes to detect cycles.
 #Perform a topological sort on the graph, assuming that there are no cycles. This gives an order in which the courses can be taken, while make sure that prerequisites are fulfilled.
-#Once the sorted order, revisit through it and ensure that all courses can be taken. If any course is encountered that hasn't been marked as completed, it return False. Otherwise, return True.
+#Once the order sorted, revisit through it and ensure that all courses can be taken. If any course is encountered that hasn't been marked as completed, it return False. Otherwise, return True.
