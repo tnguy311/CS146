377ae1bc0e32a814e5858b4ee78fa8b51b7569f8
tnguy311 <158099762+tnguy311@users.noreply.github.com>
2024-04-24 23:23:07 -0700

    Create Lab8.py

---
+++ Labs/Lab8/Lab8.py
@@ -0,0 +1,10 @@
+def coinChange(self, coins: List[int], amount: int) -> int:
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if coin <= i and dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    
+    return dp[amount] if dp[amount] != float('inf') else -1
